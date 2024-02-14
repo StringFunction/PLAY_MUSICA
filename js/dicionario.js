cd8462db14453ed8bbcc4b655383f9fe8849f335
cleciocarmais <franciscoclecioti@gmail.com>
2024-02-14 10:46:45 -0300

    sem ideia

--- js/dicionario.js
+++ js/dicionario.js
@@ -2,28 +2,28 @@ export  const musicas = [
     {   'id' : 1,
         'Nome_musica' : 'Vida Loka Parte 2 ',
         'autor' : 'Racionais',
-        'local' : '../musica/Vida Loka Parte 2.mp3',
+        'local' : 'Vida Loka Parte 2.mp3',
         'img' : '/img/3627879830_1.png'
     },
     {   
         'id' : 2,
         'Nome_musica' : 'Vida É Desafio',
         'autor' : 'Racionais',
-        'local' : '../musica/AvidaÉDesafioNadaComoUmDiaApósOOutroDiaChoraAgora.mp3',
+        'local' : 'AvidaÉDesafioNadaComoUmDiaApósOOutroDiaChoraAgora.mp3',
         'img' : '/img/racionais1.png'
     },
     {  
          'id' : 3,
         'Nome_musica' : 'Estilo Cachorro',
         'autor' : 'Racionais',
-        'local' : '../musica/Estilo Cachorro.mp3',
+        'local' : 'Estilo Cachorro.mp3',
         'img' : '/img/racionais2.png'
     },
     {  
          'id' : 4,
         'Nome_musica' : 'Vida Loka, Pt. 1',
         'autor' : 'Racionais',
-        'local' : '../musica/Vida loka parte 1 - Racionais Mcs.mp3',
+        'local' : 'Vida loka parte 1 - Racionais Mcs.mp3',
         'img' : '/img/vida_loka_parte_1.png'
     },
 
